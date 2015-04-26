8ffafa8623bf52dec49c6c034ab6b546a3b28d4a
Leonard Kleinhans <leonard.kleinhans@udo.edu>
2015-04-26 03:21:35 +0200

    Some improvements linear svm test

--- pavelsvm_pydatalog.py
+++ pavelsvm_pydatalog.py
@@ -3,21 +3,21 @@ from lp import *
 
 @pyDatalog.predicate()
 def attribute3(x, y, z):
-    yield("1", "1", "2")
-    yield("1", "2", "2")
-    yield("2", "1", "2")
-    yield("2", "2", "1")
-    yield("3", "1", "3")
-    yield("3", "2", "4")
-    yield("4", "1", "4")
-    yield("4", "2", "3")
+    yield("1", "x1", 2)
+    yield("1", "x2", 2)
+    yield("2", "x1", 2)
+    yield("2", "x2", 1)
+    yield("3", "x1", 3)
+    yield("3", "x2", 4)
+    yield("4", "x1", 4)
+    yield("4", "x2", 3)
 
 @pyDatalog.predicate()
 def label2(x, y):
-    yield('1', '-1')
-    yield("2", "-1")
-    yield("3", "1")
-    yield("4", "1")
+    yield('1', -1)
+    yield("2", -1)
+    yield("3", 1)
+    yield("4", 1)
 
 
 # Linear Program definition
@@ -29,7 +29,7 @@ print("\nBuilding a relational variant of the " + model.name)
 c = 1.0
 
 # declarations
-X, Y, Z, J, I, H, O = sub_symbols('X', 'Y', 'Z', 'J', 'I', 'H', 'O')
+X, Z, J, I = sub_symbols('X', 'Z', 'J', 'I')
 
 attribute = numeric_predicate("attribute", 2)
 
@@ -39,12 +39,10 @@ b = numeric_predicate("b", 0)
 r = numeric_predicate("r", 0)
 label = numeric_predicate("label", 1)
 
+model.add_reloop_variable(slack, weight, b, r)
 
 b_attribute = boolean_predicate("attribute", 3)
 b_label = boolean_predicate("label", 2)
-
-model.add_reloop_variable(slack, weight, b, r)
-
 
 slacks = RlpSum([I, ], b_label(I, Z), slack(I))
 innerProd = RlpSum([J, ], b_attribute(X, J, Z), weight(J) * attribute(I, J))
@@ -53,11 +51,10 @@ innerProd = RlpSum([J, ], b_attribute(X, J, Z), weight(J) * attribute(I, J))
 model += -r() + c * slacks
 
 # constraints
-
 model += ForAll([I, ], b_label(I, Z), label(I) * (innerProd + b()) + slack(I) >= r())
 
-model += ForAll([J, ], b_attribute(X, J, Z), weight(J) <= 1)
-model += ForAll([J, ], b_attribute(X, J, Z), -weight(J) <= 1)
+model += ForAll([I, ], b_attribute(X, I, Z), weight(I) <= 1)
+model += ForAll([I, ], b_attribute(X, I, Z), -weight(I) <= 1)
 model += r() >= 0
 model += ForAll([I, ], b_label(I, Z), slack(I) >= 0)
 
@@ -65,7 +62,6 @@ print("The model has been built.")
 print model
 
 model.solve()
-
 
 print("The model has been solved: " + model.status() + ".")
 
