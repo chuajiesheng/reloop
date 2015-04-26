dc19344824ec89a5520bed42a11a6ce2ec3bb9e3
Xstar <danny.heinrich@udo.edu>
2015-04-26 21:18:11 +0200

    Some testing for the Postgres Database - Creating of Tables has to be added for further testing

--- test.py
+++ test.py
@@ -171,3 +171,41 @@ for key, value in sol.iteritems():
 
 print "\nThus, the maximum flow entering the traffic network at node a is "+str(sol["flow\\2(Symbol('a'), Symbol('b'))"]+sol["flow\\2(Symbol('a'), Symbol('c'))"])+" cars per hour."
 print "\nThe total flow in the traffic network is "+str(total)+" cars per hour."
+
+
+#----------------------------------------------------------
+# SQL Testing
+
+connection =  psycopg2.connect("dbname=danny user=danny password=dark160891")
+cursor = connection.cursor()
+cursor.execute("""SELECT * FROM cost""")
+name = "cost"
+rows = cursor.fetchall()
+print rows
+for row in rows:
+    print "  ", row[0] , row[1] , row[2]
+
+query = "SELECT column_name FROM information_schema.columns where table_name=" + name
+cursor.execute("""SELECT column_name FROM information_schema.columns where table_name=\'cost\'""")
+
+columns = cursor.fetchall()
+print columns
+rlpvar =  columns.pop()
+
+print rlpvar[0]
+
+args = [('a',),('b',)]
+
+columns = zip(columns,args)
+tmpquery = []
+print columns
+for column in columns:
+    tmpquery.append(str(column[0][0]) + " = " + "\'" +str(column[1][0]) + "\'")
+
+print tmpquery
+
+tmpquery =  " AND ".join([ str(a) for a in tmpquery])
+
+query = "SELECT " + rlpvar[0] + " FROM " + name + " WHERE " +   tmpquery
+cursor.execute(query)
+print cursor.fetchall()
