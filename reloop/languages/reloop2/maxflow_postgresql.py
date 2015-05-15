e71c8fbac299cd01a8b9546ab1fe85403b618c0f
Danny Heinrich <danny.heinrich@udo.edu>
2015-05-15 12:11:54 +0200

    Added an interface for reading maxflow problems from a file [n [node] s] for source [n [node] t] for target and [a [node1] [node2] [cost]] for the edges and values

---
+++ reloop/languages/reloop2/maxflow_postgresql.py
@@ -0,0 +1,132 @@
+__author__ = 'danny'
+
+import psycopg2
+from logkb import *
+from lp import *
+import time
+
+file = open("/home/danny/Downloads/Maxflow/small/BVZ-tsukuba0.max", "r")
+
+#----------------------------------------------------------
+# SQL Testing
+
+
+#----------------------------------------------------------
+# Initialize Database with necessary Tables and Values
+dbname = raw_input("Please specifiy the name of your Database: ")
+dbname = "dbname=" + str(dbname)
+user = "user="+ raw_input("Pease specify the Username for the Database: ")
+password = "password=" + raw_input("Specify the password if applicable: ")
+connection =  psycopg2.connect(dbname + " " + user + " " + password)
+cursor = connection.cursor()
+
+
+#----------------------------------------------------------
+# Close Connection and Drop Tables
+cursor.execute("DROP TABLE IF EXISTS node2 ")
+cursor.execute("DROP TABLE IF EXISTS edge2")
+cursor.execute("DROP TABLE IF EXISTS cost2")
+cursor.execute("DROP TABLE IF EXISTS source2")
+cursor.execute("DROP TABLE IF EXISTS target2")
+connection.commit()
+
+cursor.execute("CREATE TABLE node2 (x varchar(255));")
+
+cursor.execute("CREATE TABLE edge2 (x varchar(255), y varchar(255));")
+
+cursor.execute("CREATE TABLE cost2 (x varchar(255), y varchar(255), z INTEGER NOT NULL);")
+
+cursor.execute("CREATE TABLE source2 (x varchar(255));")
+
+cursor.execute("CREATE TABLE target2 (x varchar(255));")
+
+connection.commit()
+
+file = open("/home/danny/Downloads/Maxflow/liver.n6c10.max" ,"r")
+
+count = 0
+for line in file:
+    temp = line.split()
+    if len(temp) >= 3:
+        if temp[0] == "n" and temp[2] == "s" :
+            cursor.execute("INSERT INTO source2 values('" + temp[1] + "')")
+        if temp[0] == "n" and temp[2] == "t" :
+            cursor.execute("INSERT INTO target2 values('" + temp[1] + "')")
+
+        if temp[0] == "a" :
+            cursor.execute("INSERT INTO node2 values('" + temp[1] + "')")
+            cursor.execute("INSERT INTO node2 values('" + temp[2] + "')")
+            cursor.execute("INSERT INTO edge2 values('" + temp[1] + "," + temp[2] + "')")
+            cursor.execute("INSERT INTO cost2 values('" + temp[1]+ "'" + "," + "'" +  temp[2] + "' , " + temp[3]  + ")")
+        else:
+            continue
+        count += 1
+        if count % 10000 == 0:
+            print "Reading File Please Wait ...\n " + str(count) + " Lines were read so far."
+
+    connection.commit()
+
+cursor.execute("CREATE TABLE tmp (x varchar(10));")
+cursor.execute("INSERT INTO tmp SELECT DISTINCT * FROM node2;")
+cursor.execute("DROP TABLE node2;")
+cursor.execute("ALTER TABLE tmp RENAME TO node2;")
+connection.commit()
+
+start = time.time()
+# Linear Program definition
+
+model = RlpProblem("traffic flow LP in the spirit of page 329 in http://ampl.com/BOOK/CHAPTERS/18-network.pdf",
+                   LpMaximize, PostgreSQLKb("danny","danny"), Pulp)
+
+print "\nBuilding a relational variant of the " + model.name
+
+# declarations
+X, Y, Z = sub_symbols('X', 'Y', 'Z')
+
+flow = numeric_predicate("flow", 2)
+cost = numeric_predicate("cost", 2)
+
+model.add_reloop_variable(flow)
+
+source = boolean_predicate("source", 1)
+target = boolean_predicate("target", 1)
+edge = boolean_predicate("edge", 2)
+node = boolean_predicate("node", 1)
+
+# objective
+model += RlpSum([X, Y], source(X) & edge(X, Y), flow(X, Y))
+
+# constraints for flow preservation
+outFlow = RlpSum([X, ], edge(X, Z), flow(X, Z))
+inFlow = RlpSum([Y, ], edge(Z, Y), flow(Z, Y))
+
+model += ForAll([Z, ], node(Z) & ~source(Z) & ~target(Z), inFlow |eq| outFlow)
+
+# upper and lower bound constraints
+model += ForAll([X, Y], edge(X, Y), flow(X, Y) |ge| 0)
+model += ForAll([X, Y], edge(X, Y), flow(X, Y) |le| cost(X, Y))
+
+print "The model has been built:"
+print(model)
+
+model.solve()
+
+print "\nThe model has been solved: " + model.status() + "."
+
+sol = model.get_solution()
+
+end = time.time()
+
+print "The solutions for the flow variables are:\n"
+for key, value in sol.iteritems():
+    if "flow" in key and value > 0:
+        print key+" = "+str(value)
+
+total = 0
+for key, value in sol.iteritems():
+    if "flow" in key and value > 0:
+        total += value
+
+print "\n Time needed for the grounding and solving: " + str(end - start) + " s."
+print "\nThus, the maximum flow entering the traffic network at node a is "+str(sol["flow(a, b)"]+sol["flow(a, c)"])+" cars per hour."
+print "\nThe total flow in the traffic network is "+str(total)+" cars per hour."
