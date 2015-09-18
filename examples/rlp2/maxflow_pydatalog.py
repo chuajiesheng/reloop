79e7e40d8a68fb9dc29b67e3f355fd24692a4783
Xstar <danny.heinrich@udo.edu>
2015-09-18 23:09:30 +0200

    Change assertion of pydatalog facts to prevent an error from breaking the queries

--- examples/rlp2/maxflow_pydatalog.py
+++ examples/rlp2/maxflow_pydatalog.py
@@ -10,53 +10,39 @@ from pyDatalog import pyDatalog
 A static example for the maxflow problem contained in maxflow_example.max using pyDatalog
 """
 
-@pyDatalog.predicate()
-def node1(x):
-    yield ('a')
-    yield ('b')
-    yield ('c')
-    yield ('d')
-    yield ('e')
-    yield ('f')
-    yield ('g')
+pyDatalog.assert_fact('node', 'a')
+pyDatalog.assert_fact('node', 'b')
+pyDatalog.assert_fact('node', 'c')
+pyDatalog.assert_fact('node', 'd')
+pyDatalog.assert_fact('node', 'e')
+pyDatalog.assert_fact('node', 'f')
+pyDatalog.assert_fact('node', 'g')
 
+pyDatalog.assert_fact('edge', 'a', 'b')
+pyDatalog.assert_fact('edge', 'a', 'c')
+pyDatalog.assert_fact('edge', 'b', 'd')
+pyDatalog.assert_fact('edge', 'b', 'e')
+pyDatalog.assert_fact('edge', 'c', 'd')
+pyDatalog.assert_fact('edge', 'c', 'f')
+pyDatalog.assert_fact('edge', 'd', 'e')
+pyDatalog.assert_fact('edge', 'd', 'f')
+pyDatalog.assert_fact('edge', 'e', 'g')
+pyDatalog.assert_fact('edge', 'f', 'g')
 
-@pyDatalog.predicate()
-def edge2(x, y):
-    yield ('a', 'b')
-    yield ('a', 'c')
-    yield ('b', 'd')
-    yield ('b', 'e')
-    yield ('c', 'd')
-    yield ('c', 'f')
-    yield ('d', 'e')
-    yield ('d', 'f')
-    yield ('e', 'g')
-    yield ('f', 'g')
+pyDatalog.assert_fact('source','a')
+pyDatalog.assert_fact('target', 'g')
 
+pyDatalog.assert_fact('cost', 'a', 'b', 50 )
+pyDatalog.assert_fact('cost', 'a', 'c', 100)
+pyDatalog.assert_fact('cost', 'b', 'd', 40 )
+pyDatalog.assert_fact('cost', 'b', 'e', 20 )
+pyDatalog.assert_fact('cost', 'c', 'd', 60 )
+pyDatalog.assert_fact('cost', 'c', 'f', 20 )
+pyDatalog.assert_fact('cost', 'd', 'e', 50 )
+pyDatalog.assert_fact('cost', 'd', 'f', 60 )
+pyDatalog.assert_fact('cost', 'e', 'g', 70 )
+pyDatalog.assert_fact('cost', 'f', 'g', 70 )
 
-@pyDatalog.predicate()
-def source1(x):
-    yield ('a')
-
-@pyDatalog.predicate()
-def target1(x):
-    yield ('g')
-
-
-@pyDatalog.predicate()
-def cost3(x, y, z):
-    # cost(a,b) = 50
-    yield ('a', 'b', 50)
-    yield ('a', 'c', 100)
-    yield ('b', 'd', 40)
-    yield ('b', 'e', 20)
-    yield ('c', 'd', 60)
-    yield ('c', 'f', 20)
-    yield ('d', 'e', 50)
-    yield ('d', 'f', 60)
-    yield ('e', 'g', 70)
-    yield ('f', 'g', 70)
 
 logkb = PyDatalogLogKb()
 grounder = BlockGrounder(logkb)
