99ab0b695d992d05b4d2efa1a5d3a61d1c9b9a5c
Xstar <danny.heinrich@udo.edu>
2015-09-19 17:04:16 +0200

    Assertion at top level of the LogKB and some pep8 standard refactoring

--- reloop/languages/rlp2/logkb.py
+++ reloop/languages/rlp2/logkb.py
@@ -17,13 +17,20 @@ except ImportError:
     psycopg2_available = False
 
 try:
-    from problog import *
+    import problog.tasks.probability as problog
+
+    problog_available = True
+except ImportError:
+    problog_available = False
+
+try:
+    from pyswip import Prolog
 
     prolog_available = True
 except ImportError:
     prolog_available = False
 
-assert psycopg2_available or pydatalog_available or prolog_available, 'Import Error : Please install any one of our interface Knowledgebases to proceed. Currently available are PostgreSQL and Pydatalog.'
+assert psycopg2_available or pydatalog_available or prolog_available or problog_available, 'Import Error : Please install any one of our interface Knowledgebases to proceed. Currently available are PostgreSQL and Pydatalog.'
 log = logging.getLogger(__name__)
 
 
@@ -57,7 +64,7 @@ class PyDatalogLogKb(LogKb):
     def __init__(self):
         assert pydatalog_available, "Import Error: PyDatalog is not installed on your machine. To use our PyDatalog interface please install pydatalog"
 
-    def ask(self, query_symbols, logical_query, coeff_expr = None):
+    def ask(self, query_symbols, logical_query, coeff_expr=None):
         """
         Builds a pyDataLog program from the logical_query and loads it. Then executes the query for the query_symbols.
 
@@ -75,16 +82,16 @@ class PyDatalogLogKb(LogKb):
             helper_predicate = 'helper(' + ','.join([str(v) for v in query_symbols]) + ')'
             tmp = helper_predicate + " <= " + self.transform_query(logical_query)
         else:
-            helper_len = len(query_symbols)+1
+            helper_len = len(query_symbols) + 1
             syms = OrderedSet(query_symbols)
             syms.add('COEFF_EXPR')
-            helper_predicate = 'helper(' + ','.join([str(v) for v in syms]) +')'
+            helper_predicate = 'helper(' + ','.join([str(v) for v in syms]) + ')'
             index_query = self.transform_query(logical_query)
             coeff_query = "(COEFF_EXPR == " + str(coeff_expr) + ")"
             if index_query is None:
                 tmp = helper_predicate + " <= " + coeff_query
             else:
-                tmp = helper_predicate + " <= " + " & ".join([index_query,coeff_query])
+                tmp = helper_predicate + " <= " + " & ".join([index_query, coeff_query])
         log.debug("pyDatalog query: " + tmp)
         pyDatalog.load(tmp)
         answer = pyDatalog.ask(helper_predicate)
@@ -102,8 +109,8 @@ class PyDatalogLogKb(LogKb):
         :return: The Value of the given predicate if it exists in the Database, None otherwise.
         """
         query = predicate.name + "("
-        query += ','.join([ str(a) if not isinstance(a, Symbol) else "\'" + str(a) + "\'" \
-                                    for a in predicate.args])
+        query += ','.join([str(a) if not isinstance(a, Symbol) else "\'" + str(a) + "\'" \
+                           for a in predicate.args])
         query += ", X)"
         answer = pyDatalog.ask(query)
 
@@ -131,10 +138,9 @@ class PyDatalogLogKb(LogKb):
             return " ~" + PyDatalogLogKb.transform_query(logical_query.args[0])
 
         if isinstance(logical_query, BooleanPredicate):
-            join = ",".join([str(arg) if isinstance(arg, SubSymbol) else "\'" + str(arg) + "\'"  for arg in logical_query.args])
+            join = ",".join(
+                [str(arg) if isinstance(arg, SubSymbol) else "\'" + str(arg) + "\'" for arg in logical_query.args])
             return " " + logical_query.name + "(" + join + ")"
-
-
 
         raise NotImplementedError
 
@@ -160,7 +166,7 @@ class PostgreSQLKb(LogKb):
         self.cursor = connection.cursor()
         self.recursive = True
 
-    def ask(self, query_symbols, logical_query, coeff_expr = None):
+    def ask(self, query_symbols, logical_query, coeff_expr=None):
         """
         Builds a PostgreSQL query from a given logical query and its query_symbols by implicitly joining over all given predicates.
 
@@ -187,15 +193,16 @@ class PostgreSQLKb(LogKb):
         elif isinstance(logical_query, BooleanPredicate):
             predicates.append(logical_query)
         elif isinstance(logical_query, BooleanTrue):
-            #there is no logical query here, hence we must be grounding a 
-            #single number here, e.g. the rhs of a non-forall-quantified constraint
+            # there is no logical query here, hence we must be grounding a
+            # single number here, e.g. the rhs of a non-forall-quantified constraint
             return [[coeff_expr]]
         else:
             raise NotImplementedError('The given function of the query has not been implemented yet or is not valid')
 
         column_for_symbols_where = self.get_columns_for_symbols(query_symbols, predicates)
         column_for_symbols_notexists = self.get_columns_for_symbols(query_symbols, negated_predicates)
-        column_for_symbols_all = self.get_columns_for_symbols(logical_query.atoms(Symbol), predicates + negated_predicates)
+        column_for_symbols_all = self.get_columns_for_symbols(logical_query.atoms(Symbol),
+                                                              predicates + negated_predicates)
 
         expr_as_string = str(coeff_expr)
         for symbol, columns in column_for_symbols_all.items():
@@ -204,7 +211,9 @@ class PostgreSQLKb(LogKb):
 
         query = "SELECT DISTINCT "
 
-        query += ", ".join([value[0][0] + "." + value[0][1] + " AS " + str(key) for key, value in column_for_symbols_where.items()] + [expr_as_string])
+        query += ", ".join(
+            [value[0][0] + "." + value[0][1] + " AS " + str(key) for key, value in column_for_symbols_where.items()] + [
+                expr_as_string])
         column_table_tuple_list = [value for key, value in column_for_symbols_where.items()]
         tables = set([item[0] for sublist in column_table_tuple_list for item in sublist])
 
@@ -212,7 +221,8 @@ class PostgreSQLKb(LogKb):
             queryy = "select exists(select * from information_schema.tables where table_name='" + table + "')"
             self.cursor.execute(queryy)
             if self.cursor.fetchone()[0] is False:
-                raise ValueError ("Error : the table " + str(table) + " does not exist in the specified database and therefore can not be queried.")
+                raise ValueError("Error : the table " + str(
+                    table) + " does not exist in the specified database and therefore can not be queried.")
 
         query += " FROM " + ", ".join([str(value).lower() for value in tables])
 
@@ -227,7 +237,7 @@ class PostgreSQLKb(LogKb):
             else:
                 and_clause = False
             reference_column = value[0][0] + "." + value[0][1]
-            query += " AND ".join([reference_column + " = " + rel.lower()  + "." + col for rel, col in value])
+            query += " AND ".join([reference_column + " = " + rel.lower() + "." + col for rel, col in value])
 
         query += self.and_clause_for_constants(predicates, and_clause)
 
@@ -240,7 +250,9 @@ class PostgreSQLKb(LogKb):
                     and_clause = False
                 query += " AND NOT EXISTS (SELECT * FROM " + rel.lower() + " WHERE "
                 reference_column = column_for_symbols_where[symbol][0][0]
-                query += " AND ".join([reference_column + "." + col + " = " + reltmp.lower()  + "." + col for reltmp, col in value if reltmp == rel])
+                query += " AND ".join(
+                    [reference_column + "." + col + " = " + reltmp.lower() + "." + col for reltmp, col in value if
+                     reltmp == rel])
                 query += self.and_clause_for_constants(negated_predicates, and_clause)
 
                 query += ")"
@@ -265,7 +277,8 @@ class PostgreSQLKb(LogKb):
         query = "SELECT " + str(columns[len(columns) - 1][0]) + \
                 " FROM " + str(predicate.name.lower()) + \
                 " WHERE " + \
-                " AND ".join([str(columns[index][0]) + "=" + "'" + str(arg) + "'" for index, arg in enumerate(predicate.args)])
+                " AND ".join(
+                    [str(columns[index][0]) + "=" + "'" + str(arg) + "'" for index, arg in enumerate(predicate.args)])
 
         self.cursor.execute(query)
         return self.cursor.fetchall()
@@ -288,7 +301,8 @@ class PostgreSQLKb(LogKb):
                         query += " AND "
                     else:
                         and_clause_added = True
-                    query += predicate.name.lower() + "." + self.get_column_names(predicate.name)[index] + " = " + "'" + str(arg) + "'"
+                    query += predicate.name.lower() + "." + self.get_column_names(predicate.name)[
+                        index] + " = " + "'" + str(arg) + "'"
 
         return query
 
@@ -328,27 +342,23 @@ class PostgreSQLKb(LogKb):
         ans = [item[0] for item in self.cursor.fetchall()]
         return ans
 
+
 class PrologKB(LogKb):
-
     def __init__(self, prolog):
-        try:
-            from pyswip import Prolog
-        except ImportError:
-            print("Pyswip is not available on your machine or an error has occured while trying to import the necessary modules. Please install Pyswip or fix your System Setup to use the PrologKB.")
-            exit(1)
 
-        assert isinstance(prolog,Prolog)
+        assert prolog_available, "Pyswip is not available on your machine or an error has occured while trying to import the necessary modules. Please install Pyswip or fix your System Setup to use the PrologKB."
+        assert isinstance(prolog, Prolog)
 
         self.prolog = prolog
 
     def ask_predicate(self, predicate):
 
-        result =    list(self.prolog.query(predicate.name +\
-                    "(" +\
-                    ",".join([str(arg) for index, arg in enumerate(predicate.args)]) +\
-                    ",X)"))
+        result = list(self.prolog.query(predicate.name + \
+                                        "(" + \
+                                        ",".join([str(arg) for index, arg in enumerate(predicate.args)]) + \
+                                        ",X)"))
 
-        answer=[]
+        answer = []
         for dictionary in result:
             for key, value in dictionary.items():
                 answer.append((value,))
@@ -367,57 +377,53 @@ class PrologKB(LogKb):
         prolog_answer = list(self.prolog.query(query))
         answers = []
         for dictionary in prolog_answer:
-            assert isinstance(dictionary,dict)
-            res=[]
+            assert isinstance(dictionary, dict)
+            res = []
             for query_symbol in query_symbols:
                 res.append(dictionary.get(str(query_symbol)))
             answers.append(tuple(res))
         return answers
 
-class ProbLogKB(LogKb):
 
+class ProbLogKB(LogKb):
     def __init__(self, file_path):
-        assert prolog_available, "Import Error : It seems like Problog is currently not installed or available on your machine. To proceed please install Problog"
+        assert problog_available, "Import Error : It seems like Problog is currently not installed or available on your machine. To proceed please install Problog"
         file = open(file_path, "r")
         self.knowledge = file.read()
         file.close()
 
     def execute(self, query):
-        #import subprocess
-        #proc = subprocess.Popen(["/home/danny/Workspace/Reloop/saucywrapper/problog/problog-cli.py", "prob","-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        # import subprocess
+        # proc = subprocess.Popen(["/home/danny/Workspace/Reloop/saucywrapper/problog/problog-cli.py", "prob","-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         problog_prog = self.knowledge + "\n".join(query)
         import StringIO
         import sys
 
         s = StringIO.StringIO(problog_prog)
         sys.stdin = s
-        import problog.tasks.probability as problog
 
-        result = problog.execute(filename = "-")[1]
+        result = problog.execute(filename="-")[1]
         sys.stdin = sys.__stdin__
 
         return result
 
-
-
-
-    def ask(self, query_symbols, logical_query, coeff_expr = None):
+    def ask(self, query_symbols, logical_query, coeff_expr=None):
 
         if coeff_expr is None:
             lhs_rule = 'helper(' + ','.join([str(v) for v in query_symbols]) + ')'
             rule = lhs_rule + ":-" + self.transform_query(logical_query) + "."
-            query = "query(" + lhs_rule +")."
+            query = "query(" + lhs_rule + ")."
         else:
             syms = OrderedSet(query_symbols)
             syms.add('COEFF_EXPR')
-            lhs_rule = 'helper(' + ','.join([str(v) for v in syms]) +')'
+            lhs_rule = 'helper(' + ','.join([str(v) for v in syms]) + ')'
             index_query = self.transform_query(logical_query)
             coeff_query = "COEFF_EXPR = " + str(coeff_expr) + ""
-            query = "query(" + lhs_rule +")."
+            query = "query(" + lhs_rule + ")."
             if index_query is None:
                 rule = lhs_rule + " :- " + coeff_query + "."
             else:
-                rule = lhs_rule + " :- " + " , ".join([index_query,coeff_query]) + "."
+                rule = lhs_rule + " :- " + " , ".join([index_query, coeff_query]) + "."
 
         answer = self.execute([rule, query])
 
@@ -439,10 +445,10 @@ class ProbLogKB(LogKb):
 
     def ask_predicate(self, predicate):
 
-        answer = self.execute(["query(" + predicate.name +\
-                    "(" +\
-                    ",".join([str(arg) for index, arg in enumerate(predicate.args)]) +\
-                    ",X))."])
+        answer = self.execute(["query(" + predicate.name + \
+                               "(" + \
+                               ",".join([str(arg) for index, arg in enumerate(predicate.args)]) + \
+                               ",X))."])
 
         answer_args = []
         for key in answer.keys():
@@ -463,8 +469,8 @@ class ProbLogKB(LogKb):
         if logical_query.func is And:
             return ", ".join([ProbLogKB.transform_query(arg) for arg in logical_query.args])
         if logical_query.func is Not:
-            return " not(" + ProbLogKB.transform_query(logical_query.args[0]) +")"
+            return " not(" + ProbLogKB.transform_query(logical_query.args[0]) + ")"
         if isinstance(logical_query, BooleanPredicate):
-            join = ",".join([str(arg) if isinstance(arg, SubSymbol) else str(arg)  for arg in logical_query.args])
+            join = ",".join([str(arg) if isinstance(arg, SubSymbol) else str(arg) for arg in logical_query.args])
             return " " + logical_query.name + "(" + join + ")"
         raise NotImplementedError
