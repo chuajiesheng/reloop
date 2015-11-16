9145fafe5331292e3e185a27995d4341fec621db
Leonard Kleinhans <leonard.kleinhans@udo.edu>
2015-11-16 16:22:31 +0100

    Delete this never used PostgresqlConnector

--- reloop/languages/rlp/grounding/grounder.py
+++ reloop/languages/rlp/grounding/grounder.py
@@ -22,19 +22,3 @@ class Grounder(object):
 
     def ask(self, query):
         return self.logkb.ask(query.atoms(), query)
-
-
-class PostgresqlConnector():
-    def __init__(self, db_name, db_user, db_password=None):
-        """
-        Opens a connection to the specified database and stores a cursor object for the class to access at runtime.
-
-        :param dbname: The name of the Database to connect to
-        :param user: Database User
-        :param password: The password for the given user if applicable
-        """
-        import psycopg2
-
-        connection = psycopg2.connect(
-            "dbname=" + str(db_name) + " user=" + str(db_user) + " password=" + str(db_password))
-        self.cursor = connection.cursor()
